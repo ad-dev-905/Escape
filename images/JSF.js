37b9abb87370e6dbe52cd91896a2d32eef3d772e
rakhmankulov905 <rakhmankulov905@gmail.com>
2025-10-30 15:31:12 +0600

    initial

---
+++ images/JSF.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    nav.classList.toggle('open');
+    document.body.classList.toggle('lock');
+});
